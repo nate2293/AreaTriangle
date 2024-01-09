e80b4e2c942d82f5674d0664d847c0dc807c9cdc
nate2293 <Mc_Garvey-N@ulster.ac.uk>
2024-01-09 21:30:28 +0000

    Add project files.

---
+++ AreaTriangle/TriArea.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+
+namespace AreaTriangle
+{
+    internal class TriArea
+    {
+        public static double Triangle(double triangleBase, double height)       // ****** 'Base' RESERVED key word ******
+        {
+            double area = (triangleBase * height / 2);      // eg - 12 x 10 = 120 / 2 = 60
+            return area;
+        }
+
+        static void Main()
+        {
+            Console.WriteLine(Triangle(10, 10));        // Output -  50
+            Console.WriteLine();
+
+            Console.WriteLine(Triangle(12, 10));        // Output -  60
+            Console.WriteLine();
+
+            Console.WriteLine(Triangle(6, 3));        // Output -  9
+            Console.WriteLine();
+
+        }
+    }
+}
+
+// Task - Method that takes the triangles base and height and returns the area.
